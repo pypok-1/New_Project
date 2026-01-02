cc9f8b5602baa37b5307ffb627059e6368e7ae8d
Eva <evacherkashyna9@gmail.com>
2026-01-02 13:57:00 +0200

    Update models.py

--- comments/models.py
+++ comments/models.py
@@ -1,9 +1,17 @@
 from django.db import models
-from django.conf import settings
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=1000)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
 class Comment (models.Model):
-    post = models.ForeignKey('posts.Post', on_delete=models.CASCADE)
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
