ced7c35e2159a700fcce4e8db941462ea363bb2d
Eva <evacherkashyna9@gmail.com>
2026-01-02 14:13:01 +0200

    Update views.py

--- comments/views.py
+++ comments/views.py
@@ -1,15 +1,17 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from django.core.cache import cache
-from django.shortcuts import get_object_or_404, redirect, render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
+from django.http import HttpResponseForbidden
 from .models import Comment, Post
+from django.contrib.auth.models import User
 
 
 class CommentListView(ListView):
     model = Comment
     template_name = 'comments/comment_list.html'
     paginate_by = 5
+    context_object_name = 'comments'
 
     def get_queryset(self):
         self.post = get_object_or_404(Post, id=self.kwargs['post_id'])
@@ -20,5 +22,22 @@ class CommentListView(ListView):
         context['post'] = self.post
         return context
 
+
 class CommentDetailView(DetailView):
+    model = Comment
+    template_name = 'comments/comment_detail.html'
+    pk_url_kwarg = 'id'
+    context_object_name = 'comments'
+
+
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    fields = ['text']
+    template_name = 'comments/comment_form.html'
+
+    def form_valid(self, form):
+        self.post = get_object_or_404(Post, id=self.kwargs['post_id'])
+        form.instance.post = self.post
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
