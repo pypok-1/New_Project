9ee03ec181a92d03ccdb45a901632fe137815174
Eva <evacherkashyna9@gmail.com>
2026-01-02 14:47:57 +0200

    Update urls.py

--- comments/urls.py
+++ comments/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
-from .views import CommentListView, CommentDetailView
+from .views import CommentListView, CommentDetailView, CommentCreateView, CommentUpdateView, CommentDeleteView
 
 urlpatterns = [
     path('posts/<int:post_id>/comments/', CommentListView.as_view(), name='comment_List'),
-    path('/comments/<int:id>/', CommentDetailView.as_view(), name='comment_Detail')
+    path('comments/<int:id>/', CommentDetailView.as_view(), name='comment_Detail'),
+    path('posts/<int:post_id>/comments/create/', CommentCreateView.as_view(), name='comment_Create'),
+    path('comments/<int:id>/update/', CommentUpdateView.as_view(), name='comment_Update'),
+    path('comments/<int:id>/delete/', CommentDeleteView.as_view(), name='comment_Delete')
 ]
